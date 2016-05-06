2a1130acc608959ac61fe2e35d9d45e5ac9443f4
Isaiah Peng <issaria@gmail.com>
2016-05-06 16:17:24 +0200

    use inplace transform for classdef

--- src/org/python/compiler/ScopeInfo.java
+++ src/org/python/compiler/ScopeInfo.java
@@ -19,6 +19,7 @@ public class ScopeInfo extends Object implements ScopeConstants {
     public String scope_name;
     public int level;
     public int func_level;
+    public boolean needs_class_closure;
 
     public void dump() { // for debugging
         if (org.python.core.Options.verbose < org.python.core.Py.DEBUG)
