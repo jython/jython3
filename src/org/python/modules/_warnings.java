b33eb246426840fae6e28f984e673b2426986edc
Isaiah Peng <issaria@gmail.com>
2016-06-16 17:46:36 +0200

    add _warnings.warn method

--- src/org/python/modules/_warnings.java
+++ src/org/python/modules/_warnings.java
@@ -1,6 +1,8 @@
 package org.python.modules;
 
+import org.python.core.ArgParser;
 import org.python.core.ClassDictInit;
+import org.python.core.Py;
 import org.python.core.PyObject;
 import org.python.core.PyUnicode;
 
@@ -9,14 +11,22 @@ import org.python.core.PyUnicode;
  */
 public class _warnings implements ClassDictInit {
 
-    public static final PyUnicode __doc__ = new PyUnicode("Tools that operate on functions.");
+    public static final PyUnicode __doc__ = new PyUnicode("_warnings provides basic warning filtering support.");
 
     public static void classDictInit(PyObject dict) {
-        dict.__setitem__("__name__", new PyUnicode("_functools"));
+        dict.__setitem__("__name__", new PyUnicode("_warnings"));
         dict.__setitem__("__doc__", __doc__);
 
         // Hide from Python
         dict.__setitem__("classDictInit", null);
     }
 
+    public static PyObject warn(PyObject args[], String[] keywords) {
+        ArgParser ap = new ArgParser("warn", args, keywords, "message", "category", "stacklevel");
+        String message = ap.getString(0);
+        PyObject category = ap.getPyObject(1, Py.UserWarning);
+        int stackLevel = ap.getIndex(2, 1);
+        Py.warning(category, message, stackLevel);
+        return Py.None;
+    }
 }
