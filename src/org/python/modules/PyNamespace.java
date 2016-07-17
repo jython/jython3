6b375fcebddebf4d2651901e568e46a72a8d9bdb
Isaiah Peng <issaria@gmail.com>
2016-07-17 11:08:21 +0200

    PEP-421 fix types.SimpleNamespace type

--- src/org/python/modules/PyNamespace.java
+++ src/org/python/modules/PyNamespace.java
@@ -10,48 +10,69 @@ import org.python.core.PyType;
 import org.python.core.PyUnicode;
 import org.python.expose.ExposedGet;
 import org.python.expose.ExposedMethod;
+import org.python.expose.ExposedNew;
 import org.python.expose.ExposedType;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * namespace object implementation
  */
-@ExposedType(name = "namespace", doc = BuiltinDocs.SimpleNamespace___class___doc)
+@ExposedType(name = "SimpleNamespace", doc = BuiltinDocs.SimpleNamespace_doc)
 public class PyNamespace extends PyObject {
     public static final PyType TYPE = PyType.fromClass(PyNamespace.class);
 
     @ExposedGet(name = "__dict__")
     public Map<String, PyObject> dict;
 
+    public PyNamespace(PyType subtype) {
+        super(subtype);
+        this.dict = new HashMap<>();
+    }
+
     public PyNamespace(Map<String, PyObject> dict) {
-        super();
+        super(TYPE);
         this.dict = dict;
+    }
+
+    @ExposedNew
+    @ExposedMethod(doc = BuiltinDocs.SimpleNamespace___init___doc)
+    final void SimpleNamespace___init__(PyObject[] args, String[] kwds) {
     }
 
     @Override
     public String toString() {
-        Iterable<String> keys = dict.keySet();
-        StringBuilder items = new StringBuilder();
+        StringBuilder items = new StringBuilder("namespace(");
+        boolean first = true;
         for (String key : dict.keySet()) {
-            items.append(String.format("%s=%s", key, dict.get(key)));
-            items.append(", ");
+            if (!first) {
+                items.append(", ");
+            } else {
+                first = false;
+            }
+            items.append(key).append("=").append(dict.get(key));
         }
-        return String.format("%s(%s)", getType().fastGetName(), items.substring(0, items.length() - 2));
+        return items.append(")").toString();
     }
 
-    final PyObject namespace___eq__(PyObject other) {
+    final PyObject SimpleNamespace___eq__(PyObject other) {
         return Py.newBoolean(dict.equals(other.__getattr__("__dict__")));
     }
 
     @ExposedMethod(doc = BuiltinDocs.SimpleNamespace___getattribute___doc)
-    final PyObject namespace___getattribute__(PyObject name) {
+    final PyObject SimpleNamespace___getattribute__(PyObject name) {
         return dict.get(name.asString());
     }
 
     @ExposedMethod(doc = BuiltinDocs.SimpleNamespace___setattr___doc)
-    final PyObject namespace___setattr__(PyObject name, PyObject value) {
+    final PyObject SimpleNamespace___setattr__(PyObject name, PyObject value) {
         dict.put(name.asString(), value);
         return Py.None;
+    }
+
+    @Override
+    public void __setattr__(String name, PyObject value) {
+        dict.put(name, value);
     }
 }
